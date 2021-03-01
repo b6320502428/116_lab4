8be1cf61b8ee286e9d08adc4136b24b1402de4ed
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-03-01 18:30:27 +0700

    lab4_5 up2

--- lab4_5.c
+++ lab4_5.c
@@ -17,6 +17,37 @@ int main ()
                 g[1]=g[2];
                 g[2]=tmp;
                 break;
+            case 'B':
+                tmp=g[0];
+                g[0]=g[3];
+                g[3]=tmp;
+                break;
+            case 'C':
+                tmp=g[0];
+                g[0]=g[2];
+                g[2]=tmp;
+                tmp=g[1];
+                g[1]=g[3];
+                g[3]=tmp;
+                break;
+            case 'D':
+                tmp=g[0];
+                g[0]=g[1];
+                g[1]=tmp;
+                tmp=g[2];
+                g[2]=g[3];
+                g[3]=tmp;
+                break;
+            case 'E':
+                tmp=g[0];
+                g[0]=g[3];
+                g[3]=tmp;
+                tmp=g[1];
+                g[1]=g[2];
+                g[2]=tmp;
+                break;
+            default:
+                tmp=0;
             }
         }
     }
