56d00cac17271025407aa0cc1b8b9798524af84c
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-03-01 16:58:54 +0700

    lab4_1 up3

--- lab4_1.c
+++ lab4_1.c
@@ -3,13 +3,14 @@ int main ()
 {
     int n;
     scanf("%d",&n);
-    int y[n][3],t[n-2],yt[n],b,w,i,j;
+    int y[n][3],t[n-2],yt[n],b=0,w=2000,i;
     for(i=0;i<n;i++)
     {
-        for(j=0;j<3;j++)
-        {
-            scanf("%d",&y[i][j]);
-        }
+        scanf("%d %d %d",&y[i][0],&y[i][1],&y[i][2]);
+    }
+    for(i=0;i<n;i++)
+    {
+        yt[i]=(y[i][0]*4)+(y[i][1]*2)+y[n][2];
     }
     return 0;
 }
