a64cb74cf8dea4d2280070351c63db729d50fef0
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-03-01 19:37:47 +0700

    lab4_4 up3

--- lab4_4.c
+++ lab4_4.c
@@ -2,32 +2,37 @@
 #include <string.h>
 int main ()
 {
-    int i,j=1;
-    char str[1001],ans[501];
-    scanf("%s",str);
-    ans[0]=str[0]+32;
+    int i,j=1,tmp=0;
+    char str[1001],ans[501]="";
+    scanf("%[^\n]s",str);
+    ans[0]=str[0]-32;
     for(i=1; i<strlen(str); i++)
     {
         if(str[i-1]==' ')
         {
-            if(str[i]=='i');
-            else if(str[i]=='a');
-            else if(str[i]=='o'&&str[i+1]=='f');
-            else if(str[i]=='i'&&str[i+1]=='n');
-            else if(str[i]=='o'&&str[i+1]=='n');
-            else if(str[i]=='a'&&str[i+1]=='t');
-            else if(str[i]=='a'&&str[i+1]=='n');
-            else if(str[i]=='t'&&str[i+1]=='h'&&str[i+2]=='e');
-            else if(str[i]=='f'&&str[i+1]=='o'&&str[i+2]=='r');
-            else if(str[i]=='a'&&str[i+1]=='n'&&str[i+2]=='d');
-            else if(str[i]=='w'&&str[i+1]=='i'&&str[i+2]=='t'&&str[i+3]=='h');
+            if(str[i]=='i'&&str[i+1]==' ');
+            else if(str[i]=='a'&&str[i+1]==' ');
+            else if(str[i]=='o'&&str[i+1]=='f'&&str[i+2]==' ');
+            else if(str[i]=='i'&&str[i+1]=='n'&&str[i+2]==' ');
+            else if(str[i]=='o'&&str[i+1]=='n'&&str[i+2]==' ');
+            else if(str[i]=='a'&&str[i+1]=='t'&&str[i+2]==' ');
+            else if(str[i]=='a'&&str[i+1]=='n'&&str[i+2]==' ');
+            else if(str[i]=='t'&&str[i+1]=='h'&&str[i+2]=='e'&&str[i+3]==' ');
+            else if(str[i]=='f'&&str[i+1]=='o'&&str[i+2]=='r'&&str[i+3]==' ');
+            else if(str[i]=='a'&&str[i+1]=='n'&&str[i+2]=='d'&&str[i+3]==' ');
+            else if(str[i]=='w'&&str[i+1]=='i'&&str[i+2]=='t'&&str[i+3]=='h'&&str[i+4]==' ');
+            else if(str[i]=='\0');
+            else if(str[i]=='\n');
             else
             {
-                ans[j]=str[i]+32;
+                //tmp++;
+                ans[j]=str[i]-32;
                 j++;
             }
         }
     }
-    printf("%s",ans);
+    printf("%s\n",ans);
+    //printf("%d\n",tmp);
+    //printf("%d",strlen(str));
     return 0;
 }
