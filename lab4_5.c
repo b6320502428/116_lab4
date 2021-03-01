64460e78ae405f7a9df69066ca64e1c36590d46e
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-03-01 18:26:24 +0700

    lab4_5 up1

--- lab4_5.c
+++ lab4_5.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
+#include <string.h>
 int main ()
 {
-    int n,i,j,g[4]={1,0,0,0};
+    int n,i,j,g[4]= {1,0,0,0},tmp;
     scanf("%d",&n);
     char op[51];
-    for(i=0;i<n;i++)
+    for(i=0; i<n; i++)
     {
-
+        scanf("%s",op);
+        for(j=0; j<51; j++)
+        {
+            switch(op[j])
+            {
+            case 'A':
+                tmp=g[1];
+                g[1]=g[2];
+                g[2]=tmp;
+                break;
+            }
+        }
     }
     return 0;
 }
