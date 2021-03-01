e5ab76e3f3a6fd266f299a7473dc5e277f753796
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-03-01 18:08:54 +0700

    lab4_3 create

---
+++ lab4_3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main ()
+{
+    char str[201],ans[201];
+    scanf("%s",str);
+    int i,j;
+    ans[0]=str[0];
+    for(i=0;i<201;i++)
+    {
+
+    }
+    printf("%s",ans);
+    return 0;
+}
