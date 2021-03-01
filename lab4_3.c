503a48bac71703af2c3992885ab6ebe120f695c7
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-03-01 18:13:57 +0700

    lab4_3 up1

--- lab4_3.c
+++ lab4_3.c
@@ -3,11 +3,15 @@ int main ()
 {
     char str[201],ans[201];
     scanf("%s",str);
-    int i,j;
+    int i,j=1;
     ans[0]=str[0];
-    for(i=0;i<201;i++)
+    for(i=1;i<201;i++)
     {
-
+        if(str[i]!=str[i-1])
+        {
+            ans[j]=str[i];
+            j++;
+        }
     }
     printf("%s",ans);
     return 0;
