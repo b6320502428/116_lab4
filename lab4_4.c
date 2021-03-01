5ccfe4043b8964857943e4bed59debeee76ca10d
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-03-01 19:02:20 +0700

    lab4_4 up1

--- lab4_4.c
+++ lab4_4.c
@@ -5,5 +5,13 @@ int main ()
     int i;
     char str[1001];
     scanf("%s",str);
+    for(i=1; i<1001; i++)
+    {
+        if(str[i-1]==' ')
+        {
+            if(str[i]=='i');
+            else if(str[i]=='a');
+        }
+    }
     return 0;
 }
