715ccac40f625d7faed288a36a1751132086e895
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-03-01 17:17:52 +0700

    lab4_2 up1

--- lab4_2.c
+++ lab4_2.c
@@ -4,7 +4,11 @@ int main ()
     int n;
     scanf("%d",&n);
     char p[n][7];
-    int it[n];
+    int it[n],i;
+    for(i=0;i<n;i++);
+    {
+        scanf("%s ",p[n]);
+    }
 
     return 0;
 }
